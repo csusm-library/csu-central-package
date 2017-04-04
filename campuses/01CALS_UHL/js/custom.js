e192cdf5f1de5de0a02bd51ce914c0ef3257be3f
David Walker <dwalker@calstate.edu>
2017-04-03 17:56:58 -0700

    space to tab and image link fix

--- campuses/01CALS_UHL/js/custom.js
+++ campuses/01CALS_UHL/js/custom.js
@@ -1,34 +1,31 @@
 (function() {
-  "use strict";
-  'use strict';
+	"use strict";
+	'use strict';
 
 
-  var app = angular.module('viewCustom', ['angularLoad']);
+	var app = angular.module('viewCustom', ['angularLoad']);
 
-  /****************************************************************************************************/
+	/****************************************************************************************************/
 
-  /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
+	/*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
 
-  /*var app = angular.module('centralCustom', ['angularLoad']);*/
+	/*var app = angular.module('centralCustom', ['angularLoad']);*/
 
-  /****************************************************************************************************/
+	/****************************************************************************************************/
 
-  app.controller('prmLogoAfterController', [function() {
-    var vm = this;
-    vm.getIconLink = getIconLink;
+	app.controller('prmLogoAfterController', [function() {
+		this.getIconLink = function() {
+			return this.parentCtrl.iconLink;
+		}
+	}]);
 
-    function getIconLink() {
-      return vm.parentCtrl.iconLink;
-    }
-  }]);
-
-  app.component('prmLogoAfter', {
-    bindings: {
-      parentCtrl: '<'
-    },
-    controller: 'prmLogoAfterController',
-    template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner"><a href="http://library.csueastbay.edu/"><img class="logo-image" alt="{{::(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/></a></div>'
-  });
+	app.component('prmLogoAfter', {
+		bindings: {
+			parentCtrl: '<'
+		},
+		controller: 'prmLogoAfterController',
+		template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner"><a href="http://library.csueastbay.edu/"><img class="logo-image" alt="{{::(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/></a></div>'
+	});
 
 
 
