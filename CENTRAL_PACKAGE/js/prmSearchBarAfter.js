cad6e028552d4cc6f3de355718d28c344bb91f3b
David Walker <dwalker@calstate.edu>
2017-06-05 16:11:20 -0700

    move one-search image to central package

--- CENTRAL_PACKAGE/js/prmSearchBarAfter.js
+++ CENTRAL_PACKAGE/js/prmSearchBarAfter.js
@@ -30,8 +30,33 @@ app.controller('prmSearchBarAfterController', ['$location', '$window', function(
     this.getOneSearchLogo = function() {
         var params = $location.search();
         var vid = params.vid;
+        var campus = {};
+        campus['01CALS_UBA'] = 'bakersfield';
+        campus['01CALS_UCI'] = 'channel';
+        campus['01CALS_CHI'] = 'chico';
+        campus['01CALS_UDH'] = 'dominguez';
+        campus['01CALS_UHL'] = 'eastbay';
+        campus['01CALS_UFR'] = 'fresno';
+        campus['01CALS_FUL'] = 'fullerton';
+        campus['01CALS_HUL'] = 'humboldt';
+        campus['01CALS_ULB'] = 'longbeach';
+        campus['01CALS_ULA'] = 'losangeles';
+        campus['01CALS_MAL'] = 'maritime';
+        campus['01CALS_UMB'] = 'monterey';
+        campus['01CALS_MLM'] = 'mlml';
+        campus['01CALS_UNO'] = 'northridge';
+        campus['01CALS_PUP'] = 'pomona';
+        campus['01CALS_USL'] = 'sacramento';
+        campus['01CALS_USB'] = 'sanbernardino';
+        campus['01CALS_SDL'] = 'sandiego';
+        campus['01CALS_SFR'] = 'sanfrancisco';
+        campus['01CALS_SJO'] = 'sanjose';
+        campus['01CALS_PSU'] = 'slo';
+        campus['01CALS_USM'] = 'sanmarcos';
+        campus['01CALS_SOL'] = 'sonoma';
+        campus['01CALS_UST'] = 'stanislaus';
 
-        return 'custom/' + vid + '/img/one-search.png';
+        return 'custom/CENTRAL_PACKAGE/img/one-search/' + campus[vid] + '.png';
     };
 
     console.log(this.getOneSearchLogo());
