07383ff0508744b58c24db9b4a3279b524daf095
David Walker <dwalker@calstate.edu>
2018-06-23 08:42:18 -0700

    campus adjustments for top alert

--- campuses/01CALS_FUL/js/custom.js
+++ campuses/01CALS_FUL/js/custom.js
@@ -29,10 +29,51 @@ app.component('prmLogoAfter', {
   template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner"><a href="http://library.fullerton.edu/"><img class="logo-image" alt="{{::(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/></a></div>'
 });
 
-/* app.component('prmSearchBarAfter',{
-	bindings: {},
-	template:`<div><span>Hello, World</span></div>`
+//  app.component('prmSearchBarAfter',{
+// 	bindings: {},
+// 	template:`<div><span>Hello, World</span></div>`
 
+// });
+
+
+
+
+app.run(function ($rootScope, $location, $state, $stateParams) {
+  $rootScope.$on('$locationChangeSuccess', function (event, toState, fromState) {
+      if(fromState != toState && ga) {
+          console.log("$locationChangeSuccess | event: ", event);
+          console.log("$locationChangeSuccess | referrerUrl: ", fromState);
+          console.log("$locationChangeSuccess | newUrl: ", toState);
+          console.log("$locationChangeSuccess | document.title: ", document.title);
+          // _paq.push(['setReferrerUrl', fromState]);
+          // _paq.push(['setCustomUrl', toState]);
+          // // not all pages update the title, just main and journal searches.
+          // _paq.push(['setDocumentTitle', document.title]);
+
+      /*
+          // google analytics example, but simpler but possibly not as definitive  */
+       ga('set', 'page', $location.url());
+       ga('send', 'pageview');
+ 
+        // alert( toState);
+    
+      }
+  });
 });
-*/
+
 })();
+
+//Google Analytics
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-5638015-14', 'auto');
+ga('set', 'page', $location.url());
+ga('send', 'pageview');
+
+//End Google Analytics
+
+//window.alert("Analytics Test Point 3");
--- campuses/01CALS_UDH/js/custom.js
+++ campuses/01CALS_UDH/js/custom.js
@@ -446,13 +446,14 @@ return b?(parseFloat(Sa(a,"marginLeft"))||(n.contains(a.ownerDocument,a)?a.getBo
 	(function() {
 		'use strict';
 
-		var app = angular.module('viewCustom', ['angularLoad', 'matomoAnalytics']);
+		var app = angular.module('viewCustom', ['angularLoad', 'matomoAnalytics', 'externalSearch']);
 
 		/**
 		 * Enable Google Analytics / Piwik Analytics
 		 */
 
 		angular.module('matomoAnalytics', []);
+		angular.module('externalSearch', []);
 		angular.module('matomoAnalytics').run(function ($rootScope, $interval, analyticsOptions) {
 			if(analyticsOptions.hasOwnProperty("enabled") && analyticsOptions.enabled) {
 				if(analyticsOptions.hasOwnProperty("siteId") && analyticsOptions.siteId != '' && analyticsOptions.hasOwnProperty("siteUrl") && analyticsOptions.siteUrl != '') {
@@ -631,6 +632,113 @@ return b?(parseFloat(Sa(a,"marginLeft"))||(n.contains(a.ownerDocument,a)?a.getBo
 			controller: 'prmTopbarAfterController',
 			templateUrl: 'custom/01CALS_UDH/html/prmTopbarAfterController.html'
 		});
+
+		angular.module('externalSearch').value('searchTargets', [])
+		.component('prmFacetAfter', {
+			bindings: { parentCtrl: '<' },
+			controller: ['externalSearch', function (externalSearch) {
+				this.$onInit = function () {
+					externalSearch.setController(this.parentCtrl)
+					externalSearch.addExtSearch()
+				}
+			}]
+		})
+		.component('prmPageNavMenuAfter', {
+			controller: ['externalSearch', function (externalSearch) {
+				this.$onInit = function () {
+					if (externalSearch.getController()) externalSearch.addExtSearch()
+				}
+			}]
+		})
+		.component('prmFacetExactAfter', {
+			bindings: { parentCtrl: '<' },
+			template: `
+			<div ng-if="name === \'External Search\'">
+				<div ng-hide="$ctrl.parentCtrl.facetGroup.facetGroupCollapsed">
+					<div class="section-content animate-max-height-variable">
+						<div class="md-chips md-chips-wrap">
+							<div ng-repeat="target in targets" aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4">
+								<div class="md-chip-content layout-row" role="button" tabindex="0">
+									<a class="facet-title" ng-href="{{ target.url + target.mapping(queries, filters) }}" target="_blank">
+										<strong dir="auto" title="{{ target.name }}">
+											<img ng-src="{{ target.img }}" width="22" height="22" style="vertical-align:middle;"> {{ target.name }}
+										</strong>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>`,
+			controller: ['$scope', '$location', 'searchTargets', function ($scope, $location, searchTargets) {
+				this.$onInit = function () {
+					$scope.name = this.parentCtrl.facetGroup.name
+					$scope.targets = searchTargets
+					var query = $location.search().query
+					var filter = $location.search().pfilter
+					$scope.queries = Object.prototype.toString.call(query) === '[object Array]' ? query : query ? [query] : false
+					$scope.filters = Object.prototype.toString.call(filter) === '[object Array]' ? filter : filter ? [filter] : false
+				}
+			}]
+		})
+		.factory('externalSearch', function () {
+			return {
+				getController: function () {
+					return this.prmFacetCtrl || false
+				},
+				setController: function (controller) {
+					this.prmFacetCtrl = controller
+				},
+				addExtSearch: function () {
+					var found = false;
+					this.prmFacetCtrl.facetService.results.filter(opt => opt.name === 'External Search' ? found = true : '')
+					if(!found) this.prmFacetCtrl.facetService.results.splice(3, 0, {name: 'External Search', displayedType: 'exact', limitCount: 0, facetGroupCollapsed: false, values: undefined})
+				}
+			}
+		})
+		app.value('searchTargets', [
+			{
+				"name": "Worldcat",
+				"url": "https://www.worldcat.org/search?",
+				"img": "https://www.oclc.org/etc/designs/oclc/oclc-annualreport-14/images/worldcat/wc-pinwheel.png",
+				mapping: function (queries, filters) {
+					const query_mappings = {
+						'any': 'kw',
+						'title': 'ti',
+						'creator': 'au',
+						'subject': 'su',
+						'isbn': 'bn',
+						'issn': 'n2'
+					}
+					try {
+						return 'q=' + queries.map(part => {
+							let terms = part.split(',')
+							let type = query_mappings[terms[0]] || 'kw'
+							let string = terms[2] || ''
+							let join = terms[3] || ''
+							return type + ':' + string + ' ' + join + ' '
+						}).join('')
+					}
+					catch (e) {
+						return ''
+					}
+				}
+			},
+			{
+				"name": "Google Scholar",
+				"url": "https://scholar.google.com/scholar?q=",
+				"img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/200px-Google_%22G%22_Logo.svg.png",
+				mapping: function (queries, filters) {
+					try {
+						return queries.map(part => part.split(",")[2] || "").join(' ')
+					}
+					catch (e) {
+						return ''
+					}
+				}
+			}
+		])
+
 	})();
 /* end PRIMO Angular stuffs */
 
