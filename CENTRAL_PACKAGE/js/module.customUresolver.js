aa69e5f4e7e89d036684add2f37b58757014ce1c
David Walker <dwalker@calstate.edu>
2019-04-20 09:09:24 -0700

    syntax clean-up

--- CENTRAL_PACKAGE/js/module.customUresolver.js
+++ CENTRAL_PACKAGE/js/module.customUresolver.js
@@ -23,20 +23,20 @@ angular.module('customUresolver').component('csuCustomUresolver', {
 		$scope.showExtHoldings = false;
 		_this.toggleShowItems = function (holding) {
 			holding.showItems = holding.showItems ? false : true;
-			if(_this.logToConsole) console.log(holding);
+			if (_this.logToConsole) console.log(holding);
 		}
 		_this.toggleShowCompact = function () {
 			$scope.showCompact = $scope.showCompact ? false : true;
-			if(_this.logToConsole) console.log($scope.showCompact);
+			if (_this.logToConsole) console.log($scope.showCompact);
 		}
 		_this.toggleShowExtHoldings = function () {
 			$scope.showExtHoldings = $scope.showExtHoldings ? false : true;
-			if(_this.logToConsole) console.log($scope.showExtHoldings);
+			if (_this.logToConsole) console.log($scope.showExtHoldings);
 		}
 		_this.getHoldingsCountFromBib = function (bib, vid) {
 			let count = 0;
-			for(var i = 0; i < bib.length; i++) {
-				if(bib[i].institution_code == vid.substring(0, 10)) count++;
+			for (var i = 0; i < bib.length; i++) {
+				if (bib[i].institution_code == vid.substring(0, 10)) count++;
 			}
 			return count;
 		}
@@ -51,24 +51,24 @@ angular.module('customUresolver').component('csuCustomUresolver', {
 				$scope.holdingsCount = _this.getHoldingsCountFromBib(bib, _this.vid);
 				bib.map(
 					holding => {
-						if(_this.vid.substring(0, 10) == holding.institution_code) {
+						if (_this.vid.substring(0, 10) == holding.institution_code) {
 							customUresolverService.getHoldingNote(_this.vid, holding.mms_id, holding.holding_id).then(
 								holdingnote => {
-									if(holdingnote.trim() != '') holding.ava_note = holdingnote
-									if(_this.logToConsole) console.log(holdingnote)
+									if (holdingnote.trim() != '') holding.ava_note = holdingnote
+									if (_this.logToConsole) console.log(holdingnote)
 								}
 							)
 							customUresolverService.getStatus(_this.vid, holding.mms_id, holding.holding_id).then(
 								items => {
-									if(_this.logToConsole) console.log(items)
+									if (_this.logToConsole) console.log(items)
 									holding.items = items
 									holding.showItems = false
-									if($scope.holdings.length > 0) {
-										for(var i = 0; i < $scope.holdings.length; i++) {
-											if(typeof $scope.holdings[i]['isTempHolding'] !== 'undefined') {
-												if($scope.holdings[i].mms_id == holding.mms_id) {
+									if ($scope.holdings.length > 0) {
+										for (var i = 0; i < $scope.holdings.length; i++) {
+											if (typeof $scope.holdings[i]['isTempHolding'] !== 'undefined') {
+												if ($scope.holdings[i].mms_id == holding.mms_id) {
 													let foundHolding = items.filter(function(item) {return (_this.location_code == item.temp_location) && item.in_temp_location}, $scope.holdings[i]);
-													if(foundHolding.length > 0) $scope.holdings[i].items = foundHolding;
+													if (foundHolding.length > 0) $scope.holdings[i].items = foundHolding;
 												}
 											}
 										}
@@ -76,16 +76,16 @@ angular.module('customUresolver').component('csuCustomUresolver', {
 									$scope.holdingsCount--;
 								}
 							)
-							if(holding.total_items != '') holding.numberItemsArray = new Array(parseInt(holding.total_items));
+							if (holding.total_items != '') holding.numberItemsArray = new Array(parseInt(holding.total_items));
 							$scope.holdings.push(holding)
-							if(holding.holding_id == '') {
+							if (holding.holding_id == '') {
 								holding.isTempHolding = true;
 							}
-							if(_this.logToConsole) console.log(holding)
+							if (_this.logToConsole) console.log(holding)
 						}
 					}
 				)
-				if(_this.logToConsole) console.log(bib)
+				if (_this.logToConsole) console.log(bib)
 			}
 		)
 
@@ -93,8 +93,8 @@ angular.module('customUresolver').component('csuCustomUresolver', {
 		 * Get basic holdings information from the pnx
 		 * Includes basic info for local holdings and external holdings
 		 */
-		for(var i = 0; i < _this.item.delivery.holding.length; i++) {
-			if(_this.item.delivery.holding[i].organization == _this.vid) {
+		for (var i = 0; i < _this.item.delivery.holding.length; i++) {
+			if (_this.item.delivery.holding[i].organization == _this.vid) {
 				let mms_id = _this.item.pnx.display.lds04;
 				$scope.hasLocal = true;
 				$scope.localLocations.push({
@@ -106,11 +106,11 @@ angular.module('customUresolver').component('csuCustomUresolver', {
 			} else {
 				let mms_id = _this.item.pnx.display.lds03.find(function(item) {return item.search(_this.item.delivery.holding[i].organization) >= 0 ? true : false}, _this);
 
-				if(typeof _this.tempExt['length'] == 'undefined') {
+				if (typeof _this.tempExt['length'] == 'undefined') {
 					_this.tempExt['length'] = 0
 				}
 
-				if(typeof _this.tempExt[_this.item.delivery.holding[i].organization] == 'undefined') {
+				if (typeof _this.tempExt[_this.item.delivery.holding[i].organization] == 'undefined') {
 					_this.tempExt[_this.item.delivery.holding[i].organization] = {
 						organization: _this.item.delivery.holding[i].organization,
 						locations: []
@@ -131,10 +131,10 @@ angular.module('customUresolver').component('csuCustomUresolver', {
 		 * Needs to show request options (ILL, CSU+, etc)
 		 * Currently is a work in progress, and shows nothing
 		 */
-		if(!$scope.hasLocal) {
+		if (!$scope.hasLocal) {
 			customUresolverService.getUresolver(_this.link).then(
 				data => {
-					if(_this.logToConsole) console.log(data);
+					if (_this.logToConsole) console.log(data);
 				}
 			)
 		}
@@ -255,9 +255,9 @@ function translate_campus_name(item, index) {
 }
 
 function compare_campuses(a, b) {
-  if (a.organization < b.organization)
-    return -1;
-  if (a.organization > b.organization)
-    return 1;
-  return 0;
+	if (a.organization < b.organization)
+		return -1;
+	if (a.organization > b.organization)
+		return 1;
+	return 0;
 }
