1347db24a45933c0d1b3cbea0b115a532a6da283
David Walker <dwalker@calstate.edu>
2019-06-20 08:45:23 -0700

    Move API keys to config

--- php/resolver/bibapi.php
+++ php/resolver/bibapi.php
@@ -1,5 +1,11 @@
 <?php
 
+//gets list of all courses
+//bibapi.php?get=courses&vid=01CALS_PUP
+
+//gets details for specific course
+//bibapi.php?get=course&id=2806850090002915&vid=01CALS_PUP
+
 //gets details for specific bib record
 //bibapi.php?get=bib&id=991005122479702915&vid=01CALS_PUP
 
@@ -9,13 +15,12 @@ header('Access-Control-Allow-Origin: *');
 $get = isset($_GET['get']) ? $_GET['get'] : '';
 $id = isset($_GET['id']) ? $_GET['id'] : '';
 $id2 = isset($_GET['id2']) ? $_GET['id2'] : '';
+$id3 = isset($_GET['id3']) ? $_GET['id3'] : '';
 $vid = isset($_GET['vid']) ? $_GET['vid'] : '';
 $out = '';
 
 // enhanced function for getting url data
-function en_curl($url, $opts = array("timeout" => 120, "returnheader" => FALSE, 
-    "cookie" => null, "header" => array(), 'post' => false)) 
-{
+function en_curl($url, $opts = array("timeout" => 120, "returnheader" => FALSE, "cookie" => null, "header" => array(), 'post' => false)) {
 	if(!isset($opts['timeout'])) $opts['timeout'] = 120;
 	if(!isset($opts['returnheader'])) $opts['returnheader'] = FALSE;
 	if(!isset($opts['cookie'])) $opts['cookie'] = null;
@@ -46,11 +51,10 @@ function en_curl($url, $opts = array("timeout" => 120, "returnheader" => FALSE,
 	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
 	if($opts['post'] === true) curl_setopt($curl, CURLOPT_POSTFIELDS, '');
-	//curl_setopt( $curl, CURLOPT_MAXREDIRS,      2  );
-	curl_setopt( $curl, CURLOPT_TIMEOUT, $opts['timeout']  );
+	//curl_setopt( $curl, CURLOPT_MAXREDIRS, 2);
+	curl_setopt( $curl, CURLOPT_TIMEOUT, $opts['timeout']);
 
 	// RUN THE CURL REQUEST AND GET THE RESULTS
-	$output = array();
 	$output['html'] = curl_exec($curl);
 	$output['url'] = $url;
 	$output['header'] = $header;
@@ -62,9 +66,7 @@ function en_curl($url, $opts = array("timeout" => 120, "returnheader" => FALSE,
 }
 
 //custom function for converting xml to json
-function xml2json($xml)
-{
-    $tags = array();
+function xml2json($xml) {
 	$parser = xml_parser_create();
 	xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
 	xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
@@ -104,8 +106,7 @@ function xml2json($xml)
 }
 
 //custom helper function for converting xml to json
-function recur_append($nodes, $append = array(), $isAttributes = false)
-{
+function recur_append($nodes, $append = array(), $isAttributes = false) {
 	$out = array();
 	$newnodes = array();
 	for($i = 0; $i < count($nodes); $i++) {
@@ -142,8 +143,7 @@ function recur_append($nodes, $append = array(), $isAttributes = false)
 }
 
 //custom helper function for converting xml to json
-function recur_merge($array1, $array2, $tag, $current_parents, $target_parents = array())
-{
+function recur_merge($array1, $array2, $tag, $current_parents, $target_parents = array()) {
 	foreach($array2 as $key => $value) {
 		$target_parents[] = $key;
 		$compare_parents = $current_parents == $target_parents;
@@ -244,26 +244,29 @@ function recur_merge($array1, $array2, $tag, $current_parents, $target_parents =
 	return $array1;
 }
 
-//will need to be updated to include other institutions
 //returns an apikey depending on the vid being accessed
 function getAPIKey($vid) {
-	$apikey = '';
-	switch($vid) {
-		case '01CALS_PUP_ZTEST':
-		case '01CALS_PUP':
-			$apikey =  ''; //local api key
-			break;
-		default:
-			$apikey =  ''; //network api key
-			break;
-	}
-	return $apikey;
+	$map = require_once 'config.php';
+
+    foreach ($map as $campus => $key) {
+        if (substr($vid, 0, strlen($campus)) == $campus) {
+            return $key;
+        }
+    }
+
+    return $map['default'];
 }
 
 //returns the data from the Alma API
 function getAPIData($vid, $api, $params = array()) {
 	$apiurl = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/';
 	$html = en_curl($apiurl . urldecode($api . '?format=json&apikey=' . getAPIKey($vid) . '&' . urlencode(http_build_query($params))));
+	return $html['html'];
+}
+
+//returns the data from the reserves app from the Chancellor's Office
+function getPreloadData($vid) {
+	$html = en_curl('https://reserves.calstate.edu/pomona/json');
 	return $html['html'];
 }
 
@@ -286,49 +289,53 @@ function getAVACode($subfields, $code) {
 
 //main function to get and display the correct data depending on the api requested
 switch($get) {
+	case 'courses':
+		$out = getPreloadData($vid);
+		break;
 
-	//used for course reserves module, but also might be usable for the uresolver module
+	case 'course':
+		$api = 'courses/' . $id;
+		$params = array(
+			'view' => 'full'
+		);
+		$out = getAPIData($vid, $api, $params);
+		break;
+
 	case 'bib':
 		$api = 'bibs/' . $id;
 		$params = array('expand' => 'p_avail,e_avail');
 		$out = json_decode(getAPIData($vid, $api, $params));
 		$out->bibapiurl = $api . '?format=json&apikey=&' . urlencode(http_build_query($params));
-    	$out->record = json_decode(xml2json(strstr($out->anies[0], '<record>')))->record;
-    	unset($out->anies);
+		$out->record = json_decode(xml2json(strstr($out->anies[0], '<record>')))->record;
+		unset($out->anies);
 		$api .= '/holdings';
 		$params = array('limit' => '100');
 		$holdings = json_decode(getAPIData($vid, $api, $params));
 		$out->holdingsapiurl = $api . '?format=json&apikey=&' . urlencode(http_build_query($params));
-        $out->holdings = array();
-        if(array_key_exists('holding', $holdings)) {
-            foreach($holdings->holding as $holding) {
-
-                $api .= '/' . $holding->holding_id . '/items';
-	        	$items = json_decode(getAPIData($vid, $api, $params));
+		$out->holdings = array();
+		if(array_key_exists('holding', $holdings)) {
+			foreach($holdings->holding as $holding) {
+				$api .= '/' . $holding->holding_id . '/items';
+				$items = json_decode(getAPIData($vid, $api, $params));
 				$out->itemsapiurl = $api . '?format=json&apikey=&' . urlencode(http_build_query($params));
-               if(array_key_exists('item', $items)) {
-                    foreach($items->item as $item) {
-
-                        $location = $item->holding_data->temp_location->desc;
-
-                        if($location == "") {
-                            $location = $item->item_data->location->desc;
-                        }
-
-                        $out->holdings[] = array(
-                            'mms_id' => $id,
-                            'location' => $location,
-                            'call_number' => $item->holding_data->call_number,
-                            'availability' => $item->item_data->base_status->desc,
-                            'available' => $item->item_data->base_status->value,
-                            'material_type' => strtolower($item->item_data->physical_material_type->value),
-                            'barcode' => $item->item_data->barcode,
-                            'policy' => $item->item_data->policy->value
-                        );
-                    }
-                }
-            }
-        }
+				if(array_key_exists('item', $items)) {
+					foreach($items->item as $item) {
+						$location = $item->holding_data->temp_location->desc;
+						if($location == '') $location = $item->item_data->location->desc;
+						$out->holdings[] = array(
+							'mms_id' => $id,
+							'location' => $location,
+							'call_number' => $item->holding_data->call_number,
+							'availability' => $item->item_data->base_status->desc,
+							'available' => $item->item_data->base_status->value,
+							'material_type' => strtolower($item->item_data->physical_material_type->value),
+							'barcode' => $item->item_data->barcode,
+							'policy' => $item->item_data->policy->value
+						);
+					}
+				}
+			}
+		}
 		$out = json_encode($out);
 		break;
 
@@ -337,26 +344,18 @@ switch($get) {
 		$params = array('expand' => 'p_avail,e_avail', 'mms_id' => $id);
 		$out = json_decode(getAPIData('', $api, $params));
 		$out = $out->bib[0];
-    	if(property_exists($out, 'anies')) $out->record = json_decode(xml2json(strstr($out->anies[0], '<record>')))->record;
-    	unset($out->anies);
-        $out->holdings = array();
+		if(property_exists($out, 'anies')) $out->record = json_decode(xml2json(strstr($out->anies[0], '<record>')))->record;
+		unset($out->anies);
+		$out->holdings = array();
 		$out->ava = getDatafield($out->record->datafield, 'AVA');
 		foreach($out->ava as $ava) {
 			$mms_id = getAVACode($ava, '0');
 			$holding_id = getAVACode($ava, '8');
-			$getNotes = false;
-			if($getNotes) {
-				if(!empty($holding_id)) {
-					$api2 = 'bibs/' . $mms_id . '/holdings/' . $holding_id;
-					$params2 = array('expand' => 'p_avail,e_avail');
-					$ava_note = json_decode(getAPIData($vid, $api2, $params2));
-					if(property_exists($ava_note, 'anies')) {
-						$ava_note = json_decode(xml2json(strstr($ava_note->anies[0], '<record>')))->record;
-						$ava_note = getDatafield($ava_note->datafield, '866');
-						$ava_note = (count($ava_note) > 0) ? getAVACode($ava_note, 'a') : '';
-					} else $ava_note = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/' . urldecode($api2 . '?format=json&apikey=' . getAPIKey($vid) . '&' . urlencode(http_build_query($params2)));
-				} else $ava_note = '';
-			} else $ava_note = '';
+			if(empty($holding_id)) {
+				$temp_holding_id = json_decode(getAPIData($vid, 'bibs/' . $mms_id . '/holdings/'));
+				if(property_exists($temp_holding_id, 'holding')) if(isset($temp_holding_id->holding[0])) if(property_exists($temp_holding_id->holding[0], 'holding_id')) $holding_id = $temp_holding_id->holding[0]->holding_id;
+				unset($temp_holding_id);
+			}
 			$out->holdings[] = array(
 				'mms_id' => $mms_id,
 				'holding_id' => $holding_id,
@@ -372,7 +371,8 @@ switch($get) {
 				'call_number_type' => getAVACode($ava, 'k'),
 				'priority' => getAVACode($ava, 'p'),
 				'library_name' => getAVACode($ava, 'q'),
-				'ava_note' => $ava_note
+				//'ava_raw' => $ava,
+				'ava_note' => ''
 			);
 
 		}
@@ -427,16 +427,75 @@ switch($get) {
 		$out = json_encode($items);
 		break;
 
-	//a work in progress, doesn't work right now
-	case 'uresolver':
-		$url = urldecode($id);
-		$uresolver = en_curl($url);
+	case 'checkavailable':
+		$params = json_decode($id2);
+		$params->requestType = 'ill';
+		$params->specificEdition = true;
+		$params->allowOtherLibrary = true;
+		$request = en_curl('https://' . $id . '.userservices.exlibrisgroup.com/almaws/internalRest/uresolver/institutionCode/' . $vid . '/isItemAvailableForRequest' . '?' . http_build_query($params));
+		$data['is_requestable'] = ($request['html'] == "true" || $request['html'] === true) ? true : false;
+		//$data['is_requestable_raw'] = $request['html'];
+		$data['params'] = $params;
+		$out = json_encode($data);
+		break;
+
+	case 'requestdata':
 		$data = array(
-			'physicalServicesResultId' => explode('&', explode('physicalServicesResultId=', $uresolver['html'])[1])[0],
-			'userId' => explode('&', explode('userId=', $uresolver['html'])[1])[0]
+			'request_options' => array(
+				'local' => false,
+				'resource_sharing' => false,
+				'purchase' => false,
+				'ill' => false
+			),
+			'mms_id' => '',
+			'user_id' => '',
+			'physical_services_result_id' => ''
 		);
-		$available = en_curl('https://csu-csusm.userservices.exlibrisgroup.com/almaws/internalRest/uresolver/institutionCode/01CALS_CPP/isItemAvailableForRequest?&userId=' . $data['userId'] . '&physicalServicesResultId=' . $data['physicalServicesResultId']);
-		$data['requestable'] = $available['html'];
+		$iframe_src = en_curl($id)['html'];
+		$data['request_options']['local'] = (strpos($iframe_src, 'id="openRequest"') !== false) ? true : false;
+		$data['request_options']['local_diff'] = (strpos($iframe_src, 'id="openRequestDiffIssue"') !== false) ? true : false;
+		$data['request_options']['resource_sharing'] = (strpos($iframe_src, 'id="openRSAlmaRequest"') !== false) ? true : false;
+		$data['request_options']['purchase'] = (strpos($iframe_src, 'id="openPurchaseRequest"') !== false) ? true : false;
+		$data['request_options']['ill'] = (strpos($iframe_src, 'id="openRSRequest1"') !== false) ? true : false;
+		if(strpos($iframe_src, 'getitNoHoldings') !== false) {
+			$mms_id = explode('mmsId=', $iframe_src);
+			$mms_id = explode('&', $mms_id[1]);
+			$data['mms_id'] = $mms_id[0];
+			$user_id = explode('userId=', $iframe_src);
+			$user_id = explode('&', $user_id[1]);
+			$data['user_id'] = $user_id[0];
+			$physical_services_result_id = explode('physicalServicesResultId=', $iframe_src);
+			$physical_services_result_id = explode('&', $physical_services_result_id[1]);
+			$data['physical_services_result_id'] = $physical_services_result_id[0];
+		} else {
+			$mms_id = explode('id="mmsId"', $iframe_src);
+			$mms_id = explode('value="', $mms_id[1]);
+			$mms_id = explode('"', $mms_id[1]);
+			$data['mms_id'] = $mms_id[0];
+			$user_id = explode('id="userId"', $iframe_src);
+			$user_id = explode('value="', $user_id[1]);
+			$user_id = explode('"', $user_id[1]);
+			$data['user_id'] = $user_id[0];
+			$physical_services_result_id = explode('id="physicalServicesResultId"', $iframe_src);
+			$physical_services_result_id = explode('value="', $physical_services_result_id[1]);
+			$physical_services_result_id = explode('"', $physical_services_result_id[1]);
+			$data['physical_services_result_id'] = $physical_services_result_id[0];
+		}
+		//$data['iframe_src_raw'] = $iframe_src;
+		$out = json_encode($data);
+		break;
+
+	case 'sendrequest':
+		$params = json_decode($id2);
+		$request = en_curl('https://' . $id . '.userservices.exlibrisgroup.com/view/action/uresolverRequest.do' . '?' . http_build_query($params), array('post' => true));
+		$data['request_successful'] = (strpos(strtolower($request['html']), 'request placed') !== false) ? true : false;
+		if(strpos($request['html'], 'validationFeedbacks') !== false) {
+			$error_message = explode('class="validationFeedback">', $request['html']);
+			$error_message = explode('</div>', $error_message[1]);
+			$data['error_message'] = trim($error_message[0]);
+		}
+		if(!$data['request_successful'] && !isset($data['error_message'])) $data['error_message'] = '';
+		//$data['request_raw_data'] = $request;
 		$out = json_encode($data);
 		break;
 }
