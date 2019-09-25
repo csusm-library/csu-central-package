a9df2b038d29f025bdb4165e14582053fca0377a
cooldudezach <cooldudezach@gmail.com>
2019-09-25 11:47:15 -0700

    Remove course reserves icon fix

--- CENTRAL_PACKAGE/js/prmBriefResultContainerAfterController.js
+++ CENTRAL_PACKAGE/js/prmBriefResultContainerAfterController.js
@@ -6,7 +6,7 @@ app.controller('prmBriefResultContainerAfterController', [function () {
     var vm = this.parentCtrl
 
     Object.defineProperty(vm, "isCourseDocument", {
-        get: function () { return vm.item.pnx.display.crsinfo[0].includes(vm.institutionCode); }
+        get: function () { return vm.item.pnx.display.hasOwnProperty("crsinfo") ? vm.item.pnx.display.crsinfo[0].includes(vm.institutionCode) : false; }
     });
 }]);
 
