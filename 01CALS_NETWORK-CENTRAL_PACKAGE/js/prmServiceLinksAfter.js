29e9aa9fd13935ef8424313bcd6661ec730186ef
David Walker <dwalker@calstate.edu>
2021-08-26 10:04:29 -0700

    clean-up

--- 01CALS_NETWORK-CENTRAL_PACKAGE/js/prmServiceLinksAfter.js
+++ 01CALS_NETWORK-CENTRAL_PACKAGE/js/prmServiceLinksAfter.js
@@ -8,10 +8,8 @@ app.value('linksToKeep',[]).component('prmServiceLinksAfter', {
   },
   controller: function($document, linksToKeep) {
     angular.element(function () {
-      console.log(linksToKeep);
       if (linksToKeep.length > 0) {
         var lNodes = $document[0].querySelectorAll("prm-service-links > div > div > div");
-        console.log(lNodes);
         for (var i =0;  i < lNodes.length; i++) {
           var eNode = lNodes[i];
           var span = eNode.querySelector("a > span");
